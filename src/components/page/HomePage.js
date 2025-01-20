3001bf4b174b5c0a893214b3063c7d385afd5eb5
PC <PC@DESKTOP-F726TKR>
2025-01-20 14:30:48 +0300

    adding about and language

--- src/components/page/HomePage.js
+++ src/components/page/HomePage.js
@@ -48,18 +48,48 @@ export const HomePage = () => {
                                 </DataList.Root>
                             </Card>
                         </Box>
-                        <Box style={{ marginTop: 3 }}>
-                            <Card>
-                                About
-                            </Card>
-                        </Box>
+                        <Card mt={{ md: "0", lg: "0", sm: "3" }}
+                            m={{ md: "1", lg: "2", sm: "3" }}>
+                            <Flex m={{ md: "1", lg: "2", sm: "2" }}>
+                                <Text as='div' weight='bold' size={{ md: "5" }}>About Me</Text>
+                            </Flex>
+                            <Flex m={{ md: "1", lg: "2", sm: "2" }}>
+                                <Text as='p' weight='light' size={{ sm: "2", md: "3" }}>
+                                    Am a developer who loves development and is passionate about creating impactful software. Skilled in building enterprise-level applications and delivering full-stack solutions, I thrive on solving complex problems with creativity and precision. With a deep commitment to hard work, I approach every project as an opportunity to craft something innovative and meaningful.
+                                </Text>
+                            </Flex>
+                            <Separator style={{ width: "auto" }} mt={{ lg: "4", sm: "4", md: "5", xs: "5", xl: "4" }}
+                                mb={{ lg: "4", sm: "4", md: "5", xs: "5", xl: "4" }} />
+
+                            <Flex m={{ md: "1", lg: "2", sm: "2" }}>
+                                <Text as='div' weight='bold' size={{ md: "5" }}>Language</Text>
+                            </Flex>
+                            <Flex m={{ md: "1", lg: "2", sm: "2" }}>
+                                <DataList.Root>
+                                    <DataList.Item align="center" style={{ marginLeft: 10 }}>
+                                        <DataList.Label>
+                                        Amharic: Native
+                                        </DataList.Label>
+                                    </DataList.Item>
+                                    <DataList.Item align="center" style={{ marginLeft: 10 }}>
+                                        <DataList.Label>
+                                        English: Proficient
+                                        </DataList.Label>
+                                    </DataList.Item> <DataList.Item align="center" style={{ marginLeft: 10 }}>
+                                        <DataList.Label>
+                                        Oromifaa: Conversational
+                                        </DataList.Label>
+                                    </DataList.Item>
+                                </DataList.Root>
+                            </Flex>
+                        </Card>
                     </Grid>
                 </Container>
             </Box>
             <Box>
                 <Container>
                     <Card>
-                        <Flex>Skill</Flex>
+                        <Flex m={{ md: "1", lg: "3", sm: "2" }}>Skill</Flex>
                     </Card>
                 </Container>
             </Box>
