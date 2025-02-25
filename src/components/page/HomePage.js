93d7671714e4f9e8fa427232174184fe03486474
PC <PC@DESKTOP-F726TKR>
2025-02-25 23:04:19 +0300

    changing photo

--- src/components/page/HomePage.js
+++ src/components/page/HomePage.js
@@ -18,9 +18,10 @@ export const HomePage = () => {
                             className='h-auto'>
                             <Flex>
                                 <Avatar
-                                    size="8"
-                                    src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+                                    size="9"
+                                    src="/photo.jpg"
                                     radius="full"
+                                    style={{padding:10}}
                                     fallback="logo"
                                 />
                             </Flex>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Theme appearance='inherit'>
+    <Theme appearance='light'>
       <BrowserRouter>
         {/* <Router> */}
         <Routes>
